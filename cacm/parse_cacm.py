c192ca0396f6fd41b05c7dbf55f87140677062b3
Elodie <elodie.ikkache@gmail.com>
2017-11-24 12:40:27 +0100

    index function

--- cacm/parse_cacm.py
+++ cacm/parse_cacm.py
@@ -1,29 +1,40 @@
 import re
 
-documents = []
+def segmentation(lines):
+    content = False
+    documents = []
+    for line in lines :
+        if line[:2] == ".I" :
+            documents.append({"id" : int(line[3:]), "tokens" : {}})
+        elif line[:2] in [".T", ".W", ".K"] :
+            content = True
+        elif line[0] == "." :
+            content = False
+        elif content :
+            lineContent = re.split("\W+", line)[:-1]
+            for token in lineContent :
+                if token in documents[-1]["tokens"] :
+                    documents[-1]["tokens"][token] += 1
+                else:
+                    documents[-1]["tokens"][token] = 1
+    return(documents)
+
+def index(segmentation):
+    index = {}
+    for doc in segmentation:
+        for token in doc["tokens"]:
+            if token in index:
+                index[token][0] += doc["tokens"][token]
+                index[token].append((doc["id"], doc["tokens"][token]))
+            else:
+                index[token] = [doc["tokens"][token], (doc["id"], doc["tokens"][token])]
+    return index
+
+def number_of_tokens(index):
 
 file = open("../cacm.all",'r')
-
 lines = file.readlines()
-
-content = False
-
-for line in lines :
-    if line[:2] == ".I" :
-        documents.append({"id" : int(line[3:]), "tokens" : {}})
-    elif line[:2] in [".T", ".W", ".K"] :
-        content = True
-    elif line[0] == "." :
-        content = False
-    elif content :
-        lineContent = re.split("\W+", line)[:-1]
-        #print(lineContent)
-        for token in lineContent :
-            if token in documents[-1]["tokens"] :
-                documents[-1]["tokens"][token] += 1
-            else:
-                documents[-1]["tokens"][token] = 1
-
-
+s = segmentation(lines)
+i = index(s)
 
 file.close()
