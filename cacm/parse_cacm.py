ea5b1ff26effaeb808248dadadf20138eacd5843
Quentin Rossettini <quentin.rossettini@mail.utoronto.ca>
2017-12-01 11:31:54 +0100

    number_of_tokens et size_of_vocabulary

--- cacm/parse_cacm.py
+++ cacm/parse_cacm.py
@@ -13,37 +13,48 @@ def segmentation(lines):
         elif content :
             lineContent = re.split("\W+", line)[:-1]
             for token in lineContent :
-                if token in documents[-1]["tokens"] :
-                    documents[-1]["tokens"][token] += 1
+                tokLower = token.lower()
+                if tokLower in documents[-1]["tokens"] :
+                    documents[-1]["tokens"][tokLower] += 1
                 else:
-                    documents[-1]["tokens"][token] = 1
+                    documents[-1]["tokens"][tokLower] = 1
     return(documents)
 
 def index(segmentation):
     index = {}
     for doc in segmentation:
         for token in doc["tokens"]:
-            if token in index:
-                index[token][0] += doc["tokens"][token]
-                index[token].append((doc["id"], doc["tokens"][token]))
-            else:
-                index[token] = [doc["tokens"][token], (doc["id"], doc["tokens"][token])]
+            if not (token in common):
+                if token in index:
+                    index[token][0] += doc["tokens"][token]
+                    index[token].append((doc["id"], doc["tokens"][token]))
+                else:
+                    index[token] = [doc["tokens"][token], (doc["id"], doc["tokens"][token])]
     return index
 
-def number_of_tokens(index):
+def number_of_tokens(segmentation):
     count = 0
-    for token in index:
-        count += index[token][0]
+    for doc in segmentation:
+        for token in doc["tokens"]:
+            count += doc["tokens"][token]
     return count
 
 def size_of_vocabulary(index):
     return len(index)
+
+
+def getCommonWords():
+    commonFile = open("../common_words")
+    return [s[:-1] for s in commonFile.readlines()]
+
+common = getCommonWords()
+print(common)
 
 file = open("../cacm.all",'r')
 lines = file.readlines()
 s = segmentation(lines)
 i = index(s)
 
-print(number_of_tokens(i))
+print(number_of_tokens(s))
 print(size_of_vocabulary(i))
 file.close()
