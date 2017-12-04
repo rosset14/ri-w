34c37eaa25d05ef95b4feb4fd6a8198ab2604342
Elodie <elodie.ikkache@gmail.com>
2017-12-04 11:06:12 +0100

    size of vocabulary without lemmatisation (325 985)

--- stanford/parse_stanford.py
+++ stanford/parse_stanford.py
@@ -13,6 +13,17 @@ def segmentation(lines, element):
             else:
                 documents[-1]["tokens"][tokLower] = 1
 
+def index(segmentation):
+    index = {}
+    for doc in segmentation:
+        for token in doc["tokens"]:
+            if not (token in common):
+                if token in index:
+                    index[token][0] += doc["tokens"][token]
+                    index[token].append((doc["id"], doc["tokens"][token]))
+                else:
+                    index[token] = [doc["tokens"][token], (doc["id"], doc["tokens"][token])]
+    return index
 
 def number_of_tokens(segmentation):
     count = 0
@@ -21,13 +32,34 @@ def number_of_tokens(segmentation):
             count += doc["tokens"][token]
     return count
 
+def size_of_vocabulary(index):
+    return len(index)
+
+def getCommonWords():
+    commonFile = open("../common_words")
+    return [s[:-1] for s in commonFile.readlines()]
+
 
 documents = []
+common = getCommonWords()
+#phase de segmentation des documents
+
+
 for directory in os.listdir('../../pa1-data'):
     print(str(directory))
     for element in os.listdir('../../pa1-data/' + str(directory)):
         file = open('../../pa1-data/' + str(directory)+ '/' + str(element), 'r')
         lines = file.readlines()
+        file.close()
         segmentation(lines, element)
 
-print(number_of_tokens(documents))
+"""
+for element in os.listdir('../../pa1-data/0'):
+    file = open('../../pa1-data/0/' + str(element), 'r')
+    lines = file.readlines()
+    segmentation(lines, element)
+"""
+#création de l'index inversé
+i = index(documents)
+
+print(size_of_vocabulary(i))
