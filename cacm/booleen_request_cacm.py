385cbddf97533761b34571576e050ecaec938882
Elodie <elodie.ikkache@gmail.com>
2018-01-22 18:03:57 +0100

    commentaires traduits et complétés pour les requêtes booléennes sur CACM

--- cacm/booleen_request_cacm.py
+++ cacm/booleen_request_cacm.py
@@ -2,61 +2,67 @@ import time
 from math import sqrt
 from nltk.stem.wordnet import WordNetLemmatizer  # pour la lemmatisation
 
-# import the index
+
+# IMPORT DE FICHIERS ---------------------------------------------------------------------------------------------------
+
+# import de l'index
 file = open('../index_cacm.txt', 'r')
 lines = file.readlines()
 file.close()
 index = eval(lines[0])
 
-# import the terms_id
+# import du dictionnaire des termes et de leurs ID
 file = open('../terms_cacm.txt', 'r')
 lines = file.readlines()
 file.close()
 terms_id = eval(lines[0])
 
-# import the doc_id
+# import du dictionnaire des documents et de leurs titres
 file = open('../docs_cacm.txt', 'r')
 lines = file.readlines()
 file.close()
 docs_id = eval(lines[0])
 
-
-# import the requests
+# import de requêtes booléennes pour l'évaluation
 file = open('../boolean_cacm_requests.txt', 'r')
 req = file.readlines()
 file.close()
 
-# import the answers
+# import des réponses attendues à ces requêtes
 file = open('../qrels.text', 'r')
 ans = file.readlines()
 file.close()
 
-number_of_documents = 3204 # when NOT is used, the complementary list of documents is needed
+
+number_of_documents = 3204 # quand NOT est utilisé, il faut retourner la liste complémentaire de documents
 lem = WordNetLemmatizer()
 
+# RECUPERATION DE REQUETES ---------------------------------------------------------------------------------------------
 
-# get a request from the user
+# pour avoir une requête de l'utilisateur
 def get_request():
     """
-    Asks the user for a request and takes his/her input
-    :return: the user's request
+    Pour prendre l'input de l'utisateur comme requête
+    :return: la requête de l'utilisateur
     """
     request = input("Give me a boolean request please. ex: (x AND NOT y) OR (z) ")
     return request
 
-# example of request
+# exemple de requête
 requestex = '(NOT single AND binary AND greater) OR (induces AND NOT international) OR (communicating)'
+
+# TRAITEMENT DES REQUETES ----------------------------------------------------------------------------------------------
 
 # not
 def not_request(posting):
     """
-    When a request contains a "NOT word", creates the list of documents that don't contain the word "word"
-    :param posting: the list of documents containing the word of interest
-    :return: the complementary list : the list of documents that do not contain the word of interest
+    Quand une requete contient "NOT mot", cette fonction crée la liste de ducuments qui ne contiennent pas 'mot'
+    :param posting: la liste des documents cntenant le mot d'intérêt
+    :return: la liste complémentaire, ie les documents ne contenant pas le mot d'intérêt
     """
-    list_doc = list(range(1, number_of_documents + 1)) # list of all the documents of CACM
-    if posting is None:
-        return list_doc
+    list_doc = list(range(1, number_of_documents + 1)) # liste de tous les documents de CACM
+    if posting is None:  # si un mot de la requête ne correspond à aucun mot de l'index ...
+        return list_doc  # ... on renvoie l'ensemble des documents
     for i in reversed(range(len(posting))):
         list_doc.pop(posting[i] -1)
     return list_doc
@@ -65,11 +71,12 @@ def not_request(posting):
 # and
 def and_request(posting1, posting2):
     """
-    When a request contains "V1 and V2", creates the list of documents that satisfy V1 and V2 simultaneously.
-    This version of the function doesn't implement the skip logic.
-    :param posting1: the list of documents satisfying the variable V1
-    :param posting2: the list of documents satisfying the variable V2
-    :return: the list of documents satisfy both of the variables simultaneously
+    Quand une requête contient "V1 AND V2", cette fonction créé la liste des documents qui satisfont
+    simultanément V1 et V2.
+    Cette version n'implémente pas la logique de skip.
+    :param posting1: la liste des documents qui satisfont V1
+    :param posting2: la liste des documents qui satisfont V2
+    :return: la liste des documents qui satisfont V1 et V2 simultanément
     """
     list_doc = []
     while posting1 != [] and posting2 != []:
@@ -85,19 +92,20 @@ def and_request(posting1, posting2):
 
 def and_request_skip(posting1, posting2):
     """
-    When a request contains "V1 and V2", creates the list of documents that satisfy V1 and V2 simultaneously.
-    This version of the function implements the skip logic.
-    :param posting1: the list of documents satisfying the variable V1
-    :param posting2: the list of documents satisfying the variable V2
-    :return: the list of documents satisfy both of the variables simultaneously
+    Quand une requête contient "V1 AND V2", cette fonction créé la liste des documents qui satisfont
+    simultanément V1 et V2.
+    Cette version implémente la logique de skip.
+    :param posting1: la liste des documents qui satisfont V1
+    :param posting2: la liste des documents qui satisfont V2
+    :return: la liste des documents qui satisfont V1 et V2 simultanément
     """
     n1 = int(sqrt(len(posting1)))
     n2 = int(sqrt(len(posting2)))
-    list_skip1 = []
+    list_skip1 = []  # liste des points de comparaison pour faire un skip
     while n1 < len(posting1):
         list_skip1.append(n1)
         n1 += list_skip1[0]
-    list_skip2 = []
+    list_skip2 = []  # liste des points de comparaison pour faire un skip
     while n2 < len(posting2):
         list_skip2.append(n2)
         n2 += list_skip2[0]
@@ -125,10 +133,11 @@ def and_request_skip(posting1, posting2):
 
 def or_request(posting1, posting2):
     """
-    When a request contains "V1 or V2", creates the list of documents that satisfy V1 or V2.
-    :param posting1: the list of documents satisfying the variable V1
-    :param posting2: the list of documents satisfying the variable V2
-    :return: the list of documents that satisfies at least one of the variables
+    Quand une requête contient "V1 OR V2", cette fonction créé la liste des documents qui satisfont
+    soit V1 soit V2.
+    :param posting1: la liste des documents qui satisfont V1
+    :param posting2: la liste des documents qui satisfont V2
+    :return: la liste des documents qui satisfont V1 ou V2
     """
     list_doc = []
     while posting1 != [] and posting2 != []:
@@ -145,13 +154,15 @@ def or_request(posting1, posting2):
     return list_doc + posting1 + posting2
 
 
-# manage a full request
+# gestion d'une requête entière
 
 def get_posting(word):
     """
-    Finds in the index the documents containing the word of interest
-    :param word: word of interest
-    :return: the documents containing the word of interest
+    Trouve la liste des postings correspondant à un mot dans l'index, c'est à dire la liste de documents
+    contenant le mot
+    Si le mot n'est pas dans l'index, on retourne None et on affiche un message à l'utilisateur
+    :param word: le mot d'intérêt
+    :return: la liste de documents contenant le mot d'intérêt
     """
     word = word.lower()
     word = lem.lemmatize(word)
@@ -168,9 +179,9 @@ def get_posting(word):
 
 def manage_boolean_request(req):
     """
-    From a user's request, finds the documents fulfilling it in the collection CACM
-    :param req: the user's request (str)
-    :return: the list of documents id fulfilling the request
+    A partir de la requête de l'utilisateur, on trouve la liste de documents correespondants dans la collection CACM
+    :param req: la requête de l'utilisateur (str)
+    :return: liste de documents correspondants
     """
     req_or = req.split('OR')
     total_postings = []
@@ -188,17 +199,18 @@ def manage_boolean_request(req):
             else:
                 posting = get_posting(variable)
             section_postings.append(posting)
-        suppress_None(section_postings)
-        sort_by_len(section_postings)
+        suppress_None(section_postings)  # on enlève les None qui correspondent aux mots qui ne sont pas l'index
+        sort_by_len(section_postings)  # on trie par ordre croissant pour optimiser l'algorithme
         while len(section_postings)>1:
             p1 = section_postings.pop()
             p2 = section_postings.pop()
             insert_by_len(section_postings, and_request_skip(p1, p2))
+            # and_request_skip permet d'utiliser la logique de skip, on peut utiliser and_request sinon
         if section_postings == []:
             return []
         total_postings.append(section_postings[0])
-    suppress_None(total_postings)
-    sort_by_len(total_postings)
+    suppress_None(total_postings)  # on enlève les None qui correspondent aux mots qui ne sont pas l'index
+    sort_by_len(total_postings)  # on trie par ordre croissant pour optimiser l'algorithme
     while len(total_postings) > 1:
         p1 = total_postings.pop()
         p2 = total_postings.pop()
@@ -207,6 +219,11 @@ def manage_boolean_request(req):
 
 
 def suppress_None(liste):
+    """
+    Permet de supprimer tous les None de la liste
+    :param liste:
+    :return: liste sans les None
+    """
     i = 0
     while i < len(liste):
         if liste[i] is None:
@@ -217,9 +234,9 @@ def suppress_None(liste):
 
 def sort_by_len(liste):
     """
-    Sorts a list of lists by the length of the lists
-    :param liste: list of lists
-    :return: list of lists, sorted by their length
+    Trie la liste par ordre croissant le longueur des listes contenues.
+    :param liste: liste of listes
+    :return: liste of listes, triées par ordre croissant de longueurs
     """
     n = len(liste)
     L = []
@@ -234,10 +251,10 @@ def sort_by_len(liste):
 
 def insert_by_len(L, elem):
     """
-    Insert a list in a list of list, so as to preserve the growing length of the lists
-    :param L: list of lists sorted by their length
-    :param elem: a list to be inserted
-    :return: the list with the new element, sorted by the length of its elements
+    Insere une liste dans une liste de liste de telle sorte à préserver l'ordre croissant de taille de listes
+    :param L: liste de listes triées par ordre croissant de longueur
+    :param elem: la liste qui doit être insérée
+    :return: la liste avec le nouvel élément, toujour ordonnée
     """
     n = len(L)
     i = 0
@@ -254,20 +271,30 @@ def insert_by_len(L, elem):
     return L
 
 
-# return the result to the user in a user friendly way
+# retour à l'utilisateur
 
 def request_display():
-    r = get_request()
+    """
+    Prend la requête de l'utilisateur, lance la recherche et affiche le resultat à l'utilisateur.
+    :return:
+    """
+    r = get_request()  # demande la requête
     # t1 = time.time()
-    L = manage_boolean_request(r)
+    L = manage_boolean_request(r)  # traite la requête
     # t2 = time.time()
     # print(t2 - t1)
-    print("here are the documents corresponding to your request :")
+    print("here are the documents corresponding to your request :")  # affiche le résultat
     for d in L:
         print("     document n°" + str(d) + " : " + docs_id[d])
 
 
+# EVALUATION / TESTS ---------------------------------------------------------------------------------------------------
 def eval():
+    """
+    On prend les 20 requêtes booléennes du fichier boolean_cacm_requests. On trouve les documents correspondants,
+    que l'on compare avec les réponses attendues. On peut ainsi calculer la précision et le rappel pour chaque requête.
+    :return: liste des précisions et la liste des rappels pour les 20 requêtes
+    """
     n = len(req)
     N = len(ans)
     L = []
@@ -298,8 +325,4 @@ def eval():
                     count += 1
             precision.append(count/len(calculated))
             rappel.append(count/len(supposed))
-    print(precision)
-    print(rappel)
-
-
-eval()
+    return(precision, rappel)
